33ac0ddc4b69539e5818e794cf250bd0cead5f4e
PANDA <95334270+prostoPANDA@users.noreply.github.com>
2022-03-11 22:13:05 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('html.html')
+    return ''
+
+
+@app.route('/training/<string:prof>')
+def index(prof):
+    if 'строитель' in prof.lower() or 'инженер' in prof.lower():
+        return render_template('index2.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(debug=True, port=8080, host='127.0.0.1')
